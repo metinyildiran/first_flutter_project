8629faa29a4eab105d1befef2922a55b6870c842
Metin Yıldıran <metin.1464@gmail.com>
2021-05-12 22:00:33 +0300

    Added some widgets.

--- lib/main.dart
+++ lib/main.dart
@@ -7,31 +7,43 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var ogrenciler = ["Engin Demir", "Kerem Varış", "Berkay Bilgin"];
+
     return Scaffold(
       appBar: AppBar(
         title: Text("This is an app bar"),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            var alert = AlertDialog(
-              title: Text("Sınav sonucu"),
-              content: Text("Geçti"),
-              actions: [
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Text("Tamam"),
-                )
-              ],
-            );
+      body: Column(
+        children: <Widget>[
+          Expanded(
+              child: ListView.builder(
+                  itemCount: ogrenciler.length,
+                  itemBuilder: (BuildContext context, int index) {  // This block runs for the number of itemCount
+                    return Text(ogrenciler[index]);
+                  })),
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                var alert = AlertDialog(
+                  title: Text("Sınav sonucu"),
+                  content: Text("Geçti"),
+                  actions: [
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text("Tamam"),
+                    )
+                  ],
+                );
 
-            showDialog(
-                context: context, builder: (BuildContext context) => alert);
-          },
-          child: Text("Sonucu gör"),
-        ),
+                showDialog(
+                    context: context, builder: (BuildContext context) => alert);
+              },
+              child: Text("Sonucu gör"),
+            ),
+          ),
+        ],
       ),
     );
   }
