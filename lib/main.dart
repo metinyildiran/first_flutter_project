12e021e0f154f739d091ff0669968f7454795f57
Metin Yıldıran <metin.1464@gmail.com>
2021-05-17 17:16:05 +0300

    Moved a widget to in a function.

--- lib/main.dart
+++ lib/main.dart
@@ -13,38 +13,43 @@ class MyApp extends StatelessWidget {
       appBar: AppBar(
         title: Text("This is an app bar"),
       ),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-              child: ListView.builder(
-                  itemCount: ogrenciler.length,
-                  itemBuilder: (BuildContext context, int index) {  // This block runs for the number of itemCount
-                    return Text(ogrenciler[index]);
-                  })),
-          Center(
-            child: ElevatedButton(
-              onPressed: () {
-                var alert = AlertDialog(
-                  title: Text("Sınav sonucu"),
-                  content: Text("Geçti"),
-                  actions: [
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: Text("Tamam"),
-                    )
-                  ],
-                );
+      body: buildBody(context, ogrenciler),
+    );
+  }
 
-                showDialog(
-                    context: context, builder: (BuildContext context) => alert);
-              },
-              child: Text("Sonucu gör"),
-            ),
+  Widget buildBody(BuildContext context, var ogrenciler) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+            child: ListView.builder(
+                itemCount: ogrenciler.length,
+                itemBuilder: (BuildContext context, int index) {
+                  // This block runs for the number of itemCount
+                  return Text(ogrenciler[index]);
+                })),
+        Center(
+          child: ElevatedButton(
+            onPressed: () {
+              var alert = AlertDialog(
+                title: Text("Sınav sonucu"),
+                content: Text("Geçti"),
+                actions: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text("Tamam"),
+                  )
+                ],
+              );
+
+              showDialog(
+                  context: context, builder: (BuildContext context) => alert);
+            },
+            child: Text("Sonucu gör"),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
